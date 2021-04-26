b01f2f078490bb964802e1d689103607b185f267
Your Name <you@example.com>
2021-04-26 17:00:34 +0530

    ready to deploy :rocket:

--- server.js
+++ server.js
@@ -23,6 +23,14 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex:
     })
 }).catch(err => console.log(err))
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
+    })
+}
+
 app.use('/api', authRoute)
 app.use('/api', postRoute)
 app.use('/api', userRoute)
