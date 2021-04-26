3d363a0459133f1d9e9e7d125f675377da3de69e
Your Name <you@example.com>
2021-04-26 21:14:38 +0530

    ready to deploy :rocket:

--- server.js
+++ server.js
@@ -24,12 +24,17 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex:
     })
 }).catch(err => console.log(err))
 
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'))
+//     const path = require('path')
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
+//     })
+// }
+
+
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-    const path = require('path')
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
-    })
+    app.use(express.static('client/build'));
 }
 
 app.use('/api', authRoute)
