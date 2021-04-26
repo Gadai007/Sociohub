64af60abc2b7a5bd3acd432848c1d53d1f9e5b52
Your Name <you@example.com>
2021-04-26 22:23:00 +0530

    ready to deploy :rocket:

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
-const helmet = require('helmet')
-const cors = require('cors')
 const app = express()
 const authRoute = require('./routes/authRoute')
 const postRoute = require('./routes/postRoute')
@@ -12,8 +10,6 @@ require('dotenv').config()
 
 
 app.use(cookieParser())
-app.use(cors())
-app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
