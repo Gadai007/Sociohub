6c814b0b9bb6e6804371aa97528b23622dbd673c
Your Name <you@example.com>
2021-04-26 17:22:27 +0530

    ready to deploy :rocket:

--- server.js
+++ server.js
@@ -9,13 +9,14 @@ const postRoute = require('./routes/postRoute')
 const userRoute = require('./routes/userRoute')
 require('dotenv').config()
 
+const PORT = process.env.PORT || 5000
+
 app.use(cookieParser())
 app.use(cors())
 app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-const PORT = process.env.PORT || 5000
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }).then(() => {
     app.listen(PORT, () => {
