c71259cc6fdbdb583686982f808a546ceddab241
Your Name <you@example.com>
2021-05-05 18:20:01 +0530

    code refactoring :rocket:

--- server.js
+++ server.js
@@ -1,33 +1,39 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cookieParser = require('cookie-parser')
-const path = require('path')
-const app = express()
-const authRoute = require('./routes/authRoute')
-const postRoute = require('./routes/postRoute')
-const userRoute = require('./routes/userRoute')
-const PORT = process.env.PORT || 5000
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const app = express();
+const authRoute = require("./routes/authRoute");
+const postRoute = require("./routes/postRoute");
+const userRoute = require("./routes/userRoute");
+const PORT = process.env.PORT || 5000;
+require("dotenv").config();
 
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(cookieParser())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }).then(() => {
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     app.listen(PORT, () => {
-        console.log(`server started on port ${PORT}`)
-    })
-}).catch(err => console.log(err))
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
-
-app.use('/api', authRoute)
-app.use('/api', postRoute)
-app.use('/api', userRoute)
+      console.log(`server started on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
 
 
+app.use("/api", authRoute);
+app.use("/api", postRoute);
+app.use("/api", userRoute);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    });
+  }
