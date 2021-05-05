af931b1824f65aa9309b82daecc6249d9a2b225e
Your Name <you@example.com>
2021-05-05 17:24:57 +0530

    code refactoring :rocket:

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ app.use('/api', postRoute)
 app.use('/api', userRoute)
 
 if (process.env.NODE_ENV === 'production') {
-    // app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname+'/client/build/index.html'));
-      });
+    app.use(express.static('client/build'));
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    //   });
 }
 
